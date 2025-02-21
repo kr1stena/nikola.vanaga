bdcc9ff44a5c807d75d39408276ceb33753dfd66
keita <keitazerdeva@gmail.com>
2025-02-21 12:47:28 +0200

    8564

---
+++ [XII]/21.02.25html/script.js
@@ -0,0 +1,167 @@
+alert("hello")
+let month = "Janvaris";
+switch(month.toLowerCase()){
+  
+    case "decembris":
+        alert("Ziema");
+    break;
+
+    case "janvaris":
+        alert("Ziema");
+    break;
+
+    case "februaris":
+        alert("Ziema");
+    break;
+
+    case "marts":
+        alert("Pavasaris");
+    break;
+
+    case "aprilis":
+        alert("Pavasaris");
+    break;
+
+    case "maijs":
+        alert("pavasaris");
+    break;
+
+    case "junijs":
+        alert("Vasara");
+    break;
+
+ case "julijs":
+        alert("Vasara");
+    break;
+
+    case "augusts":
+        alert("Vasara");
+    break;
+
+    case "septembris":
+        alert("Rudens");
+    break;
+
+    case "oktobris":
+        alert("Rudens");
+    break;
+
+    case "novembris":
+        alert("Rudens");
+    break;
+
+
+
+
+}
+
+let diena = 5;
+switch(diena) {
+
+case 1 :
+alert("darbdiena");
+break;
+
+case 2 :
+alert("darbdiena");
+break;
+
+case 3 :
+alert("darbdiena");
+break;
+
+case 4 :
+alert("darbdiena");
+break;
+
+case 5 :
+alert("darbdiena");
+break;
+
+case 6 :
+alert("brivdiena");
+break;
+
+case 7 :
+alert("brivdiena");
+break;
+
+
+}
+
+
+let atzime = 10:
+switch(atzime) {
+
+case 10:
+alert("labi");
+break;
+
+case 9:
+alert("labi");
+break;
+
+case 8:
+alert("labi");
+break;
+
+case 7:
+alert("videji");
+break;
+
+case 6:
+alert("videji");
+break;
+
+case 5:
+alert("videji");
+break;
+
+case 4:
+alert("slikti");
+break;
+
+case 3:
+alert("slikti");
+break;
+
+case 2:
+alert("slikti");
+break;
+
+case 1:
+alert("slikti");
+break;
+
+
+}
+
+
+let valuta = "Nauda";
+switch(valuta){
+
+case "EUR":
+    alert("Eiro");
+    break;
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
