28cb9afd1f3e6a3e19fae030f6298370426570da
keita <keitazerdeva@gmail.com>
2025-04-25 14:59:04 +0300

    vkuyjiukb

---
+++ KD02/script pd.js
@@ -0,0 +1,23 @@
+console.log ("vards:", vards);
+console.log("vecums:", vecums);
+console.log("pilsoniba:", pilsoniba);
+if(vecums) = 18 && ir pilsonis) {
+    console.log ("drikst balsot.");
+}else if (vecums ir >= 18 &&!irpilsonis){
+    console.log("personal ir $(vecums)} gadi, bet vina nav pilsonis.");
+}else if (vecums < 18 && irpilsonis){
+
+
+}console.log("persona nav pilngadiga, bet ir pilsonis")
+} else {
+    console.log("persona nav pilngadiga um nav  pilsonis.");
+
+
+
+
+
+
+
+
+
+    
